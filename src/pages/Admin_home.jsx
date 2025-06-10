15d62aef30b8789b614c088ffc1e9746bcf58b4c
deep473 <deepztanshu@gmail.com>
2025-06-10 11:20:39 +0530

    10th june

--- src/pages/Admin_home.jsx
+++ src/pages/Admin_home.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 export default function Admin_home() {
   return (
-    <div>Admin_home</div>
+    <>
+      <h3>Welcome to Admin Home</h3>
+
+      <NavLink to = "/pm">Product management</NavLink>
+      <NavLink to = "/um">User management</NavLink>
+    </>
   )
 }

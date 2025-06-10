584808b34684bd8ce28b584bd7accc12b107719f
deep473 <deepztanshu@gmail.com>
2025-06-10 10:12:59 +0530

    nav-l1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import Sign_up from "./pages/Sign_up";
+import { Route, Routes } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Sign_up from "./pages/Sign_up"
+import Sign_in from "./pages/Sign_in"
 
 function App() {
 
   return (
     <>
-      <Sign_up />
+      <Routes>
+          <Route path = "/" element = {<Welcome />} />
+          <Route path = "/sign_up" element = {<Sign_up />} />
+          <Route path = "/sign_in" element = {<Sign_in />} />
+      </Routes>
     </>
   )
 }
--- src/pages/Sign_in.jsx
+++ src/pages/Sign_in.jsx
@@ -1,18 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Sign_in() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const data = {
+      username,
+      password
+    };
+
+    try {
+      const resp = await fetch('http://localhost:8080/signIn', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+
+      const msg = await resp.text();
+      alert(msg);
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Failed to submit data");
+    }
+  }
+
   return (
     <>
-        <h4>Sign in below:</h4>
-        <form>
-            <label>Username: </label>
-            <input type = "text" name = "username"></input>
-            <br></br><br></br>
-            <label>Password: </label>
-            <input type = "password" name = "password"></input>
-            <br></br><br></br>
-            <input type = "submit" value = "SIGN IN"></input>
-        </form>
+      <h4>Sign in below</h4>
+      <form onSubmit={handleSubmit}>
+        <label>Username: </label>
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br /><br />
+
+        <label>Password: </label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br /><br />
+
+        <button type="submit">SIGN IN</button>
+      </form>
     </>
-  )
+  );
 }
